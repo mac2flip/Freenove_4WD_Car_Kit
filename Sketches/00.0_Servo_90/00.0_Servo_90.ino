4b9e2129d95c0999a8ef6993ccd544e606102c8f
mac2flip <109447931+mac2flip@users.noreply.github.com>
2026-03-15 20:30:44 -0700

    servo left middle right middle test

--- Sketches/00.0_Servo_90/00.0_Servo_90.ino
+++ Sketches/00.0_Servo_90/00.0_Servo_90.ino
@@ -7,15 +7,32 @@
 #include <Servo.h>      //servo library
 
 #define PIN_SERVO  2    //define servo pin
+#define PIN_LED    13   //UNO built-in LED
 
 Servo servo;            //create servo object to control a servo
-byte servoOffset = 0;   // change the value to Calibrate servo
+int servoOffset = 0;    // signed trim in degrees for center calibration
 
 void setup() {
   servo.attach(PIN_SERVO);        //initialize servo
-  servo.write(90 + servoOffset);  //Calibrate servo
+  pinMode(PIN_LED, OUTPUT);
+  servo.write(90 + servoOffset);  //Start at center position
+  delay(600);
 }
 
 void loop() {
-  while (true);
+  digitalWrite(PIN_LED, HIGH);
+  servo.write(60 + servoOffset);
+  delay(700);
+
+  digitalWrite(PIN_LED, LOW);
+  servo.write(90 + servoOffset);
+  delay(700);
+
+  digitalWrite(PIN_LED, HIGH);
+  servo.write(120 + servoOffset);
+  delay(700);
+
+  digitalWrite(PIN_LED, LOW);
+  servo.write(90 + servoOffset);
+  delay(700);
 }
